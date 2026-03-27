8e9d1b3f141a5643ee6a5b215d9170663796cad2
Havish Karthikeya Kanamarlapudi <havishkanamarlapudi@gmail.com>
2026-03-27 08:28:33 +0530

    Update admin password in constants.ts

--- constants.ts
+++ constants.ts
@@ -4,7 +4,7 @@ export const INITIAL_POEMS: Poem[] = [];
 
 export const ADMIN_CREDENTIALS = {
   email: 'admin@echopages.com',
-  password: '**********'
+  password: 'Havish@2006'
 };
 
 export const ADMIN_EMAILS = [ADMIN_CREDENTIALS.email];
